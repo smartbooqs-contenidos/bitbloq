9b7c9e45882f0b9c6f57dfd50488ca420bda7114
smartbooqs-contenidos <155761161+smartbooqs-contenidos@users.noreply.github.com>
2024-01-05 18:16:01 +0100

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,9 @@
+import requests
+
+html = requests.get("https://bitbloq.cc").text
+
+html = html.replace("/_next/", "https://bitbloq.cc/_next/")
+
+file = open("docs/index.html", "w")
+file.write(html)
+file.close()
