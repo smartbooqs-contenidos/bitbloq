0a291edf83b0e4af81f71bcba42862e465e9e3b4
smartbooqs-contenidos <155761161+smartbooqs-contenidos@users.noreply.github.com>
2024-01-05 17:54:15 +0000

    index

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import requests
 
 html = requests.get("https://bitbloq.cc").text
 
-# html = html.replace("/_next/", "https://bitbloq.cc/_next/")
+html = html.replace("/_next/", "https://bitbloq.cc/_next/")
 
 file = open("docs/index.html", "w")
 file.write(html)
