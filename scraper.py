0f2eb997a2b092012a562432ecd579f638a65b62
SmartBooqs <155761161+smartbooqs-contenidos@users.noreply.github.com>
2024-01-15 10:55:50 +0100

    replace icons url

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import requests
 html = requests.get("https://bitbloq.cc").text
 
 html = html.replace("/_next/", "https://bitbloq.cc/_next/")
+html = html.replace("/icons/", "https://bitbloq.cc/icons/")
 
 file = open("docs/index.html", "w")
 file.write(html)
